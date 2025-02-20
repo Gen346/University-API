e74448276c3f3e9b3d7828ef3ed1ae90f87e3ca8
yxvnyk <gobbleddygook92@gmail.com>
2025-02-20 16:01:11 +0200

    Ad MS test for ApplicationDbContext. test check correctnes of configuration string property

---
+++ API practice 2Tests/Data/ApplicationDBContextTests.cs
@@ -0,0 +1,15 @@
+﻿namespace API_practice_2.Data.Tests
+{
+    [TestClass()]
+    public class ApplicationDBContextTests
+    {
+        [TestMethod()]
+        public void ConnectionStringIsCorrect()
+        {
+            ApplicationDBContext dBContext = new ApplicationDBContext();
+            string dbConnectionString = dBContext.connectionString;
+            string correctConnectionString = "Server = 38.180.138.70; Database = University;User Id = root; Password = G8P8v46P74; Encrypt = True; TrustServerCertificate = True";
+            Assert.AreEqual(correctConnectionString,  dbConnectionString);
+        }
+    }
+}

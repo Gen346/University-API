48aa04f24826463476eeaf80bfd03978bf734500
Gen346 <kelzreez@gmail.com>
2025-02-21 22:01:13 +0200

    Epta

--- API practice 2/Data/ApplicationDBContext.cs
+++ API practice 2/Data/ApplicationDBContext.cs
@@ -1,6 +1,25 @@
-﻿namespace API_practice_2.Data
+﻿using API_practice_2.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API_practice_2.Data
 {
-    public class ApplicationDBContext
+    public class ApplicationDBContext : DbContext
     {
+        private readonly IConfiguration _configuration;
+
+        public ApplicationDBContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public DbSet<University> Univeristies { get; set; }
+        public DbSet<Faculty> Faculties { get; set; }
+        public DbSet<Speciality> Specialities { get; set; }
+        public DbSet<Group> Groups {  get; set; }
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(_configuration.GetConnectionString("VPS"));
+        }
     }
 }
